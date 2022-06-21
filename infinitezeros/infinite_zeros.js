1eecf1988e9441a002e96447a423a42eb0378d08
enducube <lskull238@gmail.com>
2022-06-21 12:05:06 +1200

    basic stuff and testing

---
+++ infinitezeros/infinite_zeros.js
@@ -0,0 +1,19 @@
+// Infinite Zeros, by enducube.
+
+var money = 0;
+
+function save() {
+    var saveGame = {
+        money: money,
+    }
+    document.cookie = "save="+JSON.stringify(saveGame)+
+}
+
+function load(){
+    var loadGame = JSON.parse(document.cookie)
+
+}
+
+
+
+setInterval(save, 60000)
---
+++ wordlebutcool/cool.js
@@ -0,0 +1,17 @@
+//wooderl cool
+
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+      result = xmlhttp.responseText;
+    }
+    return result;
+  }
+
+words = loadFile("wordlebutcool/words.txt").split("\n");
+words.array.forEach(element => {
+    document.getElementsByName("body").appendChild("<p>"+element+"</p>");
+});
